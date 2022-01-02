8b5c3cfafcf1ea7480fb0d2196fc12d38431f1bf
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-01-02 16:42:10 -0500

    Updated page style

--- client/src/MainPage/Components/NavBar/NavBar.jsx
+++ client/src/MainPage/Components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <div>
         <nav>
-            <img id="nav-logo" src="../LogoKWH.png" alt="" />
+            <img id="nav-logo" src="https://user-images.githubusercontent.com/43398149/147889780-9da97ef8-d4b6-423a-acc8-b976291f1ecd.png" alt="" />
             <SearchBar />
         </nav>
         </div>
--- client/src/MainPage/Routes/Update/Update.jsx
+++ client/src/MainPage/Routes/Update/Update.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {useParams} from 'react-router-dom'
+import NavBar from '../../Components/NavBar/NavBar';
 
 function Update() {
+    const params = useParams();
+    const propertyId = Number(params.id);
+    console.log(propertyId);
     return (
         <div>
+            <NavBar />
             Update
         </div>
     )
