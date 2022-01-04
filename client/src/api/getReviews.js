36c211ec91806934501b8dd81f1548ba2bbb48e9
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-01-04 00:57:28 -0500

    added reviews API

---
+++ client/src/api/getReviews.js
@@ -0,0 +1,36 @@
+const baseURL = 'http://localhost:5000/api/v1/reviews';
+
+const getReviews = {
+    async getAllReviews() {
+        const response = await fetch(baseURL);
+        const jsonResponse = await response.json();
+        const result = jsonResponse.data.reviews;
+        return result;
+    },
+    async getReviewById(id) {
+        const newURL = `${baseURL}/${id}`;
+        const response = await fetch(newURL);
+        const jsonResponse = await response.json();
+        const result = jsonResponse.data.reviews;
+        return result;
+    },
+    async addReview(review) {
+        const body = {
+            name: review.name, 
+            rating: review.rating, 
+            review: review.review,
+            property_id: review.property_id
+        };
+        const response = await fetch(baseURL, {
+            method: 'POST',
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            body: JSON.stringify(body)
+        });
+        const jsonResponse = await response.json();
+        const result = jsonResponse.status;
+        return result;
+    }
+}
+
+export default getReviews;
+
