361f40ae452a804d69841cc6f20788b403104845
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-01-04 14:07:17 -0500

    updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import LandingPage from './LandingPage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import Details from './MainPage/Routes/Details/Details';
-import Update from './MainPage/Routes/Update/Update';
 import { PropertiesContextProvider } from './Context/PropertiesContext';
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
                         <Route exact path="/" element={<LandingPage />} />
                         <Route exact path="/properties" element={<MainPage />} />
                         <Route exact path="/properties/:id" element={<Details />} />
-                        <Route exact path="/properties/:id/update" element={<Update />} />
                     </Routes>
                 </Router>
             </PropertiesContextProvider>
