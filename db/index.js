9146370bd00c05bf2277b2c7e927150c5e88dcef
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-01-04 16:46:14 -0500

    added ssl to production config

--- db/index.js
+++ db/index.js
@@ -17,6 +17,9 @@ const devConfig = {
 
 const proConfig =  {
     connectionString: process.env.DATABASE_URL
+    ssl: {
+        rejectUnauthorized: false,
+    }
 }
 
 const pool = new Pool(process.env.NODE_ENV === 'production'? proConfig : devConfig);
