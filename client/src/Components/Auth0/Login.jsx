7463989fcc1918c7943d319c89a780667667dcc6
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-24 13:24:29 -0400

    Removed bootstrap

--- client/src/Components/Auth0/Login.jsx
+++ client/src/Components/Auth0/Login.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
+import './ButtonStyles.css'
 
 const LoginButton = () => {
   const { loginWithPopup } = useAuth0();
 
-  return <Button 
-  variant="dark"
-  style={{marginRight: '3rem'}}
-  onClick={() => loginWithPopup()}>Log In</Button>;
+  return <button 
+  // variant="dark"
+  className="auth0-button"
+  
+  onClick={() => loginWithPopup()}>
+    <b><p>Log In</p></b>
+  </button>;
   
 };
 
--- client/src/Components/Auth0/Logout.jsx
+++ client/src/Components/Auth0/Logout.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import './ButtonStyles.css'
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <Button 
-    variant="dark"
-    style={{marginRight: '3rem'}}
+    <button 
+    // variant="dark"
+    className="auth0-button"
     onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
-    </Button>
+      <b><p>Log Out</p></b>
+    </button>
   );
 };
 
