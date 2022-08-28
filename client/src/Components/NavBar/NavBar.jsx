8e244258a2bf86faca47d1b03ab880e871f9c298
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-28 19:11:44 -0400

    Removed redundant console log

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -6,7 +6,6 @@ import Logout from '../Auth0/Logout';
 
 function NavBar() {
     const {isAuthenticated} = useAuth0();
-    console.log(window.location.origin);
     return (
         <nav>
             <a href=".."><img id="nav-logo" src="https://user-images.githubusercontent.com/43398149/147889780-9da97ef8-d4b6-423a-acc8-b976291f1ecd.png" alt="" /></a>
