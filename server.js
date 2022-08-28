8f00cbbb87b80f6ca4a1640a6b160f714e5c53c7
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-28 10:27:32 -0400

    Removed static file serving

--- server.js
+++ server.js
@@ -17,11 +17,9 @@ app.use(morgan("dev"));
 
 const port = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, "/client/build")));
-// }
-
-app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+}
 
 const Properties = express.Router();
 
