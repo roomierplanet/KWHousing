22be54516a1370807f7be3e47758aee4628e3072
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-29 10:02:28 -0400

    Removed ability to add properties

--- client/src/Routes/MainPage/MainPage.jsx
+++ client/src/Routes/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './MainPage.css';
 import NavBar from '../../Components/NavBar/NavBar';
 import PropertyList from '../../Components/PropertyList/PropertyList';
-import AddProperty from '../../Components/AddProperty/AddProperty';
+// import AddProperty from '../../Components/AddProperty/AddProperty';
 
 function MainPage() {
     return (
