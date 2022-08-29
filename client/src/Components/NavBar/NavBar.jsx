c379883885f408fec4f8d14d0a33e61a8766856c
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2022-08-29 15:58:39 -0400

    Fixed missing CSS

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ import Login from '../Auth0/Login';
 import Logout from '../Auth0/Logout';
 
 function NavBar() {
-    const {isAuthenticated} = useAuth0();
+    const {isLoading, isAuthenticated} = useAuth0();
     return (
         <nav>
             <a href=".."><img id="nav-logo" src="https://user-images.githubusercontent.com/43398149/147889780-9da97ef8-d4b6-423a-acc8-b976291f1ecd.png" alt="" /></a>
             <SearchBar />
-            {isAuthenticated ? <Logout></Logout> : <Login></Login> }
+            {!isLoading && isAuthenticated ? <Logout></Logout> : <Login></Login> }
         </nav>
     )
 }
