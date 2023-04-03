70a6cbd0a6237838c092caf911f2c50c08a7758f
Arnav Nagpal <a7nagpal@uwaterloo.ca>
2023-04-03 15:46:44 -0400

    Add properties allowed for root user

--- client/src/Routes/MainPage/MainPage.jsx
+++ client/src/Routes/MainPage/MainPage.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './MainPage.css';
 import NavBar from '../../Components/NavBar/NavBar';
 import PropertyList from '../../Components/PropertyList/PropertyList';
-// import AddProperty from '../../Components/AddProperty/AddProperty';
+import { useAuth0 } from "@auth0/auth0-react";
+import AddProperty from '../../Components/AddProperty/AddProperty';
 
 function MainPage() {
+    const {user} = useAuth0();
     return (
         <div>
             <NavBar />
-            {/* <AddProperty /> */}
+            {user && user.sub ==='google-oauth2|117396793460697106457' && <AddProperty />}
             <PropertyList />
         </div>
     )
